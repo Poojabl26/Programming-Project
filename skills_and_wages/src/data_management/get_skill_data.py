4a35e4549afbf2b8d89624e9a98915d59b577e0a
Pooja bansal <pooja.bansal2610@gmail.com>
2019-02-18 21:16:57 +0100

    Merged the files

--- skills_and_wages/src/data_management/get_skill_data.py
+++ skills_and_wages/src/data_management/get_skill_data.py
@@ -15,3 +15,10 @@ vp = pd.read_stata(ppj("IN_DATA","vpenglish.dta"))
 other = pd.read_stata(ppj("IN_DATA","vpgenenglish.dta"))
 job = pd.read_stata(ppj("IN_DATA","vpequiv.dta"))
 cog = pd.read_stata(ppj("IN_DATA","cognitenglish.dta"))
+
+# Merging all the files and locating the relevant columns
+file = pd.merge(pd.merge(pd.merge(cog, vp, on='persnr'), other, on='persnr'), job, on='persnr')
+file = file.loc[:,
+       ['persnr', 'vp62', 'vbilzeit', 'egp88_05', 'f96t90g', 'f99z90r', 'vp12501', 'vp12502', 'vp12503', 'vp12504',
+        'vp12505', 'vp12506', 'vp12507', 'vp12508', 'vp12509', 'vp12510', 'vp12511', 'vp12512', 'vp12513',
+        'vp12514', 'vp12515', 'd1110905', 'd1110105', 'ijob105']]
