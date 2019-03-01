1541be04f7b2b2d2d216ab0cf5ba14ce287df1d6
Pooja bansal <pooja.bansal2610@gmail.com>
2019-03-01 12:05:31 +0100

    Defining functions for determining the trre recursively

--- skills_and_wages/src/analysis/regression.py
+++ skills_and_wages/src/analysis/regression.py
@@ -180,3 +180,90 @@ def number_of_positives(dflocal):
         else:
             number_of_negatives += 1
     return number_of_positives, number_of_negatives
+
+# Determines the tree recursively by finding the best node heuristically
+def Create_tree_TDIDT(node_list, dfa, current_node_id, tree_depth):
+    current_node = node_list[current_node_id]
+
+    rowaxes, columnaxes = dfa.axes
+    pplus, pminus = number_of_positives(dfa)
+
+    network_information_gain = 0
+    final_mean = 0
+    node_attribute = 0
+    final_cutpoint = 0
+
+    for current_column in range(len(columnaxes) - 1):
+        df_temp = dfa.sort_values(by=[columnaxes[current_column]])
+        sorted_array = df_temp[:][columnaxes[current_column]]
+        result = df_temp[:][columnaxes[-1]]
+        # print(result)
+        pinnerplus = 0
+        pinnerminus = 0
+        max_information_gain = 0
+        prev_out = 2
+        for i in range(len(rowaxes)):
+            if (df_temp.iat[i, -1] == 1.0):
+                pinnerplus += 1
+                information_gain = get_information_gain(ppos=pinnerplus, pneg=pinnerminus,
+                                                        npos=(pplus - pinnerplus), nneg=(pminus - pinnerminus))
+                if (information_gain > max_information_gain):
+                    max_information_gain = information_gain
+                    potential_cutpoint = i
+                    if i > 0:
+                        potential_mean = (df_temp.iat[i, current_column] +
+                                          df_temp.iat[i - 1, current_column]) / 2;
+                    else:
+                        potential_mean = df_temp.iat[i, current_column];
+            else:
+                pinnerminus += 1
+        if (max_information_gain > network_information_gain):
+            network_information_gain = max_information_gain
+            node_attribute = current_column
+            final_mean = potential_mean
+            final_cutpoint = potential_cutpoint
+    #    print('network_information_gain',network_information_gain)
+    #    print('node_attribute',node_attribute)
+    #    print('final_mean',final_mean)
+    #    print('final_cutpoint',final_cutpoint)
+    #    print(columnaxes[node_attribute])
+    #    print('-----------------------------')
+
+    # Updating the current array
+    current_node.threshold = final_mean
+    current_node.pplus = pplus
+    current_node.pminus = pminus
+    current_node.label = columnaxes[node_attribute]
+    # The array is sorted and split
+    df_temp = dfa.sort_values(by=[columnaxes[node_attribute]])
+    df1 = df_temp.iloc[:final_cutpoint, :]
+    df2 = df_temp.iloc[final_cutpoint:, :]
+
+    if pplus == 0 or pminus == 0 or final_cutpoint == 0 or tree_depth >= 3:
+        current_node.is_leaf = True
+        current_node.outcome = (pplus > pminus)
+        return
+    else:
+        current_node.is_leaf = False
+
+    left_node = TDIDTNode(current_node_id)
+    right_node = TDIDTNode(current_node_id)
+
+    current_node.left_child_id = len(node_list)
+    current_node.right_child_id = len(node_list) + 1
+
+    # only needed to fullfill exercise requirements
+    left_node.identifier = current_node.left_child_id
+    right_node.identifier = current_node.right_child_id
+    left_node.parent_test_outcome = "yes"
+    right_node.parent_test_outcome = "no"
+
+    node_list.append(left_node)
+    node_list.append(right_node)
+    node_list[current_node.left_child_id].identifier = current_node.left_child_id;
+    node_list[current_node.right_child_id].identifier = current_node.right_child_id;
+    #    node_list[current_node.left_child_id].is_Left = True
+    Create_tree_TDIDT(node_list, df1, current_node.left_child_id, tree_depth + 1)
+    Create_tree_TDIDT(node_list, df2, current_node.right_child_id, tree_depth + 1)
+
+    return df_temp
